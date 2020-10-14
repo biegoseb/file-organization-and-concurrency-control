6445844e744d1fb995bb876e149b4e47e25f710d
Diego <diego@archlinux.localdomain>
2020-10-14 03:47:58 -0500

    Prev failed

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -7,6 +7,7 @@
 #include <functional>
 #include <sstream>
 #include <algorithm>
+#include "../sequential-file/sale-record.hpp"
 #include "../sequential-file/animal-record.hpp"
 
 using std::string;
@@ -14,22 +15,25 @@ using std::vector;
 using std::function;
 using std::fstream;
 using std::stringstream;
+using std::stoi;
+using std::stod;
+using std::stold;
 using std::stoll;
 using std::ios;
 
 class Parser {
-    vector<AnimalRecord>* animal_records;
+    vector<SaleRecord>* sale_records;
     //
-    AnimalRecord* create_animal_record(const string& info);
+    SaleRecord* create_sale_record(const string& info);
 public:
     Parser(const string& file_name);
     ~Parser();
-    vector<AnimalRecord>* get_animal_records() const;
+    vector<SaleRecord>* get_sale_records() const;
     //
     
 };
 
-AnimalRecord* Parser::create_animal_record(const string& info) {
+SaleRecord* Parser::create_sale_record(const string& info) {
     vector<string> fields;
     stringstream ss(info);
     string field;
@@ -37,36 +41,39 @@ AnimalRecord* Parser::create_animal_record(const string& info) {
         fields.push_back(field);
     }
 
-    AnimalRecord* animal_record;
-    if (fields.size() == 10) {
-        string country_area = fields[0];
-        string year = fields[1];
-        long long int comm_code = stoll(fields[2]);
-        string commodity = fields[3];
-        string flow = fields[4];
-        long long int trade_usd = stoll(fields[5]);
-        long long int weight_kg = stoll(fields[6]);
-        string quantity_name = fields[7];
-        long long int quantity = stoll(fields[8]);
-        string category = fields[9];
+    SaleRecord* sale_record;
+    if (fields.size() == 13) {
+        string country = fields[0];
+        string region = fields[1];   
+        string item_type = fields[2];
+        string channel = fields[3];
+        string order_date = fields[4];
+        string ship_date = fields[5];
+        string order_id = fields[6];
+        unsigned int units_sold = stoi(fields[7]);
+        double unit_price = stod(fields[8]);
+        double unit_cost = stod(fields[9]);
+        long double total_revenue = stold(fields[10]);
+        long double total_cost = stold(fields[11]);
+        long double total_profit = stold(fields[12]);
         
-        animal_record = new AnimalRecord(country_area, year, comm_code, 
-                                commodity, flow, trade_usd, weight_kg, 
-                                quantity_name, quantity, category);
+        sale_record = new SaleRecord(country, region, item_type, channel, 
+            order_date, ship_date, order_id, units_sold, unit_price, unit_cost, 
+            total_revenue, total_cost, total_profit);
     }
-    return animal_record;
+    return sale_record;
 }
 
 Parser::Parser(const string& file_name) { 
-    animal_records = new vector<AnimalRecord>();
+    sale_records = new vector<SaleRecord>();
 
     fstream* file = new fstream(file_name, ios::in);
     string line;
     getline(*file, line);
     if (file){
         while (getline(*file, line)){
-            AnimalRecord* rec_temp = create_animal_record(line);
-            animal_records->push_back(*rec_temp);
+            SaleRecord* rec_temp = create_sale_record(line);
+            sale_records->push_back(*rec_temp);
         }
         file->close();
     }
@@ -74,12 +81,12 @@ Parser::Parser(const string& file_name) {
 }
 
 Parser::~Parser() {
-    delete animal_records;
+    delete sale_records;
     //
 }
 
-vector<AnimalRecord>* Parser::get_animal_records() const {
-    return animal_records;
+vector<SaleRecord>* Parser::get_sale_records() const {
+    return sale_records;
 }
 
 #endif // PARSER_HPP
