70e850a74bdd415a3cb47ae2a12d1c84dd6c4146
luisejv <luis.jauregui@utec.edu.pe>
2020-10-16 05:22:35 -0500

    insert done

--- Hash/final_hash.cpp
+++ Hash/final_hash.cpp
@@ -2,43 +2,47 @@
 #include <vector>
 #include <unordered_map>
 #include <fstream>
+#include <string>
+#include <cstring>
+//#include "../sequential-file/sale-record.hpp"
 
 using namespace std;
 
-struct Record {
-    string country_or_area;
-    string year;
-    string comm_code;
-    string commodity;
-    string flow;
-    string trade_usd;
-    string weight_kg;
-    string quantity_name;
-    string quantity;
-    string category;
-};
+struct SaleRecord {
+    char country[35];
+    char region[35];
+    char item_type[20];
+    char channel[8];
+    char order_date[12];
+    char ship_date[12];
+    char order_id[12];
+    unsigned int units_sold;
+    double unit_price;
+    double unit_cost;
+    long double total_revenue;
+    long double total_cost;
+    long double total_profit;
 
-struct Record2 {
-    string zip_code;
-    string county_name;
-    string state_name; 
-    string name; 
-    string addrs1; 
-    string city; 
-    string abbrev; 
-    string zip; 
-    string web_addr; 
-    string textcond; 
-    string sp_textcond; 
-    string description; 
-    string over65; 
-    string under65; 
-    string na_available; 
-    string community; 
-    string issue; 
-    string attained; 
-    string na_rating;
-    string last_mod;
+    SaleRecord(){};
+
+    SaleRecord(char country[35], char region[35], char item_type[20], 
+    char channel[8], char order_date[12], char ship_date[12], char order_id[12],
+    unsigned int units_sold, double unit_price, double unit_cost, 
+    long double total_revenue, long double total_cost, long double total_profit) {
+        strcpy(this->country, country);
+        strcpy(this->region, region);
+        strcpy(this->item_type, item_type);
+        strcpy(this->channel, channel);
+        strcpy(this->order_date, order_date);
+        strcpy(this->ship_date, ship_date);
+        strcpy(this->order_id, order_id);
+        this->units_sold = units_sold;
+        this->unit_price = unit_price;
+        this->unit_cost = unit_cost;
+        this->total_revenue = total_revenue;
+        this->total_cost = total_cost;
+        this->total_profit = total_profit;
+    }
 };
 
 struct Bucket
@@ -47,72 +51,74 @@ struct Bucket
     int local_depth;
     Bucket* next_bucket;
     vector<pair<string, long>> registros;
-    /* string name; */
+    string name;
+    Bucket(){};
 };
 
 template <typename R>
 class Hash{
     private:
-        unordered_map<string, Bucket> hash;
-        fstream archIndex;
+        unordered_map<string, Bucket*> hash;
         fstream archData;
-        string archName;
         string archDataName;
         int max_global_depth;
         int global_depth;
         int block_factor;
-        /* string key; */
+        int cont_split;
     public:
-        Hash(string archIndexName, string dataName)
+        Hash(string dataName)
         {
-            archName = archIndexName;
             archDataName = dataName;
             cout << "Inserte factor de bloque: ";
             cin >> block_factor;
             cout << "Inserte maximo de nivel global: ";
             cin >> max_global_depth;
             global_depth = 1;
-            /* hash["0"] = 0;
-            hash["1"] = sizeof(Bucket); 
-            archIndex.open(archName, ios::binary | ios::out); */
-            Bucket zero;
-            Bucket one;
-            zero.next_bucket = nullptr;
-            one.next_bucket = nullptr;
-            zero.size = 0;
-            one.size = 0;
-            zero.local_depth = 1;
-            one.local_depth = 1;
+            Bucket* zero = new Bucket();
+            Bucket* one = new Bucket();
+            zero->next_bucket = nullptr;
+            one->next_bucket = nullptr;
+            zero->size = 0;
+            one->size = 0;
+            zero->local_depth = 1;
+            one->local_depth = 1;
+            zero->name = "0";
+            one->name = "1";
             hash["0"] = zero;
             hash["1"] = one;
         }
+        
         void insert(R registro, string key){
             fstream removedData;
-            removedData.open("eliminados.dat", ios::binary | ios::ate);
+            removedData.open("eliminados.dat", ios::binary | ios::in);
             long address = -1;
             int size = 0;
-            if (removedData)
+            if (removedData.is_open())
             {
-                file.seekg(0, ios::end);
-                size = (int)file.tellg();
-                file.seekg(0, ios::beg);
+                removedData.seekg(0, ios::end);
+                size = (int)removedData.tellg();
+                removedData.seekg(0, ios::beg);
             }
             if (size > 0){
+                cout << "holi" << endl;
                 int valid = 0;
                 int cont =-1;
                 while (valid == 0 & !removedData.eof())
                 {
-                    removedData.read((char *)address, sizeof(address));
-                    removedData.read((char *)valid, sizeof(valid));
+                    removedData.read((char *)&address, sizeof(address));
+                    removedData.read((char *)&valid, sizeof(valid));
                     cont++;
                 }
                 if (valid != 0){
                     valid = 0;
+                    removedData.close();
+                    removedData.open("eliminados.dat", ios::binary | ios::out);
                     removedData.seekg(cont * (sizeof(long) + sizeof(int)) + sizeof(long));
-                    removedData.write((char *)valid, sizeof(valid));
-                    archData.open(archDataName, ios::binary | ios::ate);
+                    removedData.write((char *)&valid, sizeof(valid));
+                    archData.open(archDataName, ios::binary | ios::in);
                     archData.seekg(address);
                 }
+                cout << size << " " << address << endl;
             }
             if (size == 0 || address == -1) {
                 archData.open(archDataName, ios::binary | ios::app);
@@ -123,7 +129,7 @@ class Hash{
             removedData.close();
             string hashCode = hashing(key);
             string newHash;
-            Bucket bucket;
+            Bucket* bucket;
             int contDepth = 0;
             bool encontrado = false;
             while (contDepth < global_depth && !encontrado)
@@ -136,46 +142,65 @@ class Hash{
                     encontrado = true;
                 }
             }
-            bool overflow = false;
-            while (bucket.next_bucket != nullptr)
+            while (bucket->next_bucket != nullptr)
             {
-                bucket = *(bucket.next_bucket);
-                overflow = true;
+                bucket = bucket->next_bucket;
             }
-            if (bucket.size < block_factor){
-                bucket.size++;
-                bucket.registros.push_back(make_pair(hashCode, address));
-            } else {
-                if (bucket.local_depth < max_global_depth) {
-                    Bucket newBucket;
-                    newBucket.local_depth = ++bucket.local_depth;
-                    newBucket.next_bucket = nullptr;
+            if (bucket->size < block_factor){
+                bucket->size = bucket->size + 1;
+                bucket->registros.push_back(make_pair(hashCode, address));
+                cout << "se inserto en " << bucket->name << endl;
+            }
+            else
+            {
+                bool inserted = false;
+                while (bucket->local_depth < max_global_depth && !inserted)
+                {
+                    Bucket *newBucket = new Bucket();
+                    newBucket->local_depth = ++bucket->local_depth;
+                    newBucket->next_bucket = nullptr;
+                    string baseHash = bucket->name;
                     string hashToReallocate;
-                    string newHash0 = "0" + newHash;
-                    string newHash1 = "1" + newHash;
-                    for (int i = 0; i < bucket.size; i++)
+                    string newHash0 = "0" + baseHash;
+                    string newHash1 = "1" + baseHash;
+                    for (int i = 0; i < bucket->size; i++)
                     {
-                        hashToReallocate = bucket.registros[i].first;
-                        if (hashToReallocate.substr(hashToReallocate.length() - bucket.local_depth) == newHash1){
-                            newBucket.registros.push_back(bucket.registros[i]);
-                            bucket.registros.erase(bucket.registros.begin() + i);
-                            bucket.size--;
-                            newBucket.size++;
+                        hashToReallocate = bucket->registros[i].first;
+                        if (hashToReallocate.substr(hashToReallocate.length() - bucket->local_depth) == newHash1){
+                            newBucket->registros.push_back(bucket->registros[i]);
+                            bucket->registros.erase(bucket->registros.begin() + i);
+                            bucket->size--;
+                            newBucket->size++;
                         }
                     }
-                    hash.erase(newHash);
+                    bucket->name = newHash0;
+                    newBucket->name = newHash1;
+                    hash.erase(baseHash);
                     hash[newHash0] = bucket;
                     hash[newHash1] = newBucket;
-                    if (global_depth < bucket.local_depth){
+                    cont_split++;
+                    if (global_depth < bucket->local_depth){
                         global_depth++;
                     }
-                } else {
-                    Bucket newBucket;
-                    newBucket.local_depth = bucket.local_depth;
-                    newBucket.next_bucket = -1;
-                    newBucket.size = 1;
-                    newBucket.registros.push_back(make_pair(hashCode, address));
-                    bucket.next_bucket = newBucket;
+                    string newHashCode = hashCode.substr(hashCode.length() - bucket->local_depth);
+                    auto bucketToInsert = hash[newHashCode];
+                    if (bucketToInsert->size < block_factor)
+                    {
+                        inserted = true;
+                        bucketToInsert->registros.push_back(make_pair(hashCode, address));
+                        cout << "se inserto en " << bucketToInsert->name << endl;
+                        return;
+                    }
+                }
+                if (!inserted){
+                    Bucket *newBucket = new Bucket();
+                    newBucket->local_depth = bucket->local_depth;
+                    newBucket->next_bucket = nullptr;
+                    newBucket->size = 1;
+                    newBucket->registros.push_back(make_pair(hashCode, address));
+                    newBucket->name = bucket->name + "+";
+                    cout << "se inserto en " << newBucket->name << endl;
+                    bucket->next_bucket = newBucket;
                 }
             }
         };
@@ -196,70 +221,135 @@ class Hash{
         };
 
         void remove(string key){
-            string hashBinary = hashing(key);
-            long posInArch = -1;
-            string pos ; //ṕosicion de hash[DeBucket]
-            int contDepth = 0;
+            string hashCode = hashing(key);
             bool encontrado = false;
+            int contDepth = 0;
+            string prevHash;
+            Bucket *bucket;
             while (contDepth < global_depth && !encontrado)
             {
                 contDepth++;
-                // 101010 length = 4, substr(length - 3) = '010'
-                pos = hashBinary.substr(hashBinary.length() - contDepth);
-                if (hash.find(pos) != hash.end())
+                prevHash = hashCode.substr(hashCode.length() - contDepth);
+                if (hash.find(prevHash) != hash.end())
                 {
-                    posInArch = hash[pos];
+                    bucket = hash[prevHash];
                     encontrado = true;
                 }
             }
-
-
+            fstream removedData;
+            removedData.open("eliminados.dat", ios::binary | ios::app);
+            Bucket *prevBucket;
+            bool overflow = false;
+            bool deleted = false;
+            pair<string, long> registro;
+            while (bucket->next_bucket != nullptr && !deleted)
+            {
+                prevBucket = bucket;
+                int i = 0;
+                while(i < bucket->registros.size() && !deleted){
+                    registro = bucket->registros[i];
+                    if (registro.first == hashCode)
+                    {
+                        bucket->registros.erase(bucket->registros.begin() + i);
+                        if (bucket->size == 1){
+                            if (overflow){
+                                prevBucket->next_bucket = nullptr;
+                                delete bucket;
+                            } else {
+                                string newHash = prevHash.substr(1, prevHash.size() - 1);
+                                string indexToRemove = (prevHash[0] == '0') ? "1" + newHash : "0" + newHash;
+                                prevBucket = hash[indexToRemove];
+                                hash.erase(indexToRemove);
+                                hash.erase(prevHash);
+                                prevBucket->local_depth--;
+                                hash[newHash] = prevBucket;
+                                cont_split--;
+                                if (cont_split == 0)
+                                {
+                                    global_depth--;
+                                }
+                            }
+                        }
+                        bucket->size--;
+                        deleted = true;
+                    }
+                    i++;
+                }
+                overflow = true;
+                bucket = bucket->next_bucket;
+            }
+            if (removedData){
+                int valid = 1;
+                cout << registro.second << endl;
+                removedData.write((char *)&registro.second, sizeof(long));
+                removedData.write((char *)&valid, sizeof(int));
+            }
+            removedData.close();
         }
         
         R search(string key){
-            string hashBinary = hashing(key);
-            long posInArch = -1;
-            string pos ; //ṕosicion de hash[DeBucket]
-            int contDepth = 0;
+            R registro;
+            string hashCode = hashing(key);
             bool encontrado = false;
+            int contDepth = 0;
+            string hashToSearch;
+            Bucket *bucket;
             while (contDepth < global_depth && !encontrado)
             {
                 contDepth++;
-                // 101010 length = 4, substr(length - 3) = '010'
-                pos = hashBinary.substr(hashBinary.length() - contDepth);
-                if (hash.find(pos) != hash.end())
+                hashToSearch = hashCode.substr(hashCode.length() - contDepth);
+                if (hash.find(hashToSearch) != hash.end())
                 {
-                    posInArch = hash[pos];
+                    bucket = hash[hashToSearch];
                     encontrado = true;
                 }
             }
-            if (encontrado) {
-                archIndex.open(archName, ios::in | ios::binary);
-                archIndex.seekg(posInArch);
-                R registro;
-                long posinData;
-                Bucket bucket;
-
-                archIndex.read((char *)&bucket, sizeof(bucket));                
-                archIndex.close();
-                for (int i= 0; i< BLOCK_FACTOR ;i++){
-                    posinData = bucket.registros[i];
-                    archData.open(archDataName, ios::binary | ios::app);
-                    archData.seekg(posinData);
-                    archData.read((char *)&registro, sizeof(registro));
-                    if (registro.id == key){
+            encontrado = false;
+            while (bucket->next_bucket != nullptr) {
+                int i = 0;
+                while (i < bucket->registros.size() && !encontrado)
+                {
+                    if (bucket->registros[i].first == hashCode)
+                    {
+                        archData.open(archDataName, ios::binary | ios::in);
+                        archData.seekg(bucket->registros[i].second);
+                        archData.read((char *)&registro, sizeof(registro));
+                        encontrado = true;
                         archData.close();
-                        return registro;                               
+                        return registro;
                     }
+                    i++;
                 }
-            } else {
-                cerr << "No se encontro la busqueda";
-                return NULL;
+                if (!encontrado){
+                    bucket = bucket->next_bucket;
+                }
             }
-        }; 
+            cerr << "No se encontro el registro" << endl;
+            return registro;
+        };
+
+        void saveHash(){
+
+        }
 };
 
 int main(){
-    Hash<Record> extensibleHash ("example.dat", "arch.csv");
+    Hash<SaleRecord> extensibleHash ("data-temp2.dat");
+    SaleRecord record1 ((char*)"Ciego", (char*)"ciego", (char*)"item_type", (char*)"channel", (char*)"01/02/2000", (char*)"01/02/2000", (char*)"123456", 500, 345.2, 234.3, 1231.2, 1345.3, 324235.3);
+    SaleRecord record2 ((char*)"Biego", (char*)"biego", (char*)"item_type", (char*)"channel", (char*)"01/02/2000", (char*)"01/02/2000", (char*)"123456", 500, 345.2, 234.3, 1231.2, 1345.3, 324235.3);
+    SaleRecord record3 ((char*)"Aiego", (char*)"aiego", (char*)"item_type", (char*)"channel", (char*)"01/02/2000", (char*)"01/02/2000", (char*)"123456", 500, 345.2, 234.3, 1231.2, 1345.3, 324235.3);
+    SaleRecord record4 ((char*)"Diego", (char*)"diego", (char*)"item_type", (char*)"channel", (char*)"01/02/2000", (char*)"01/02/2000", (char*)"123456", 500, 345.2, 234.3, 1231.2, 1345.3, 324235.3);
+    SaleRecord record7 ((char*)"Eiego", (char*)"diego", (char*)"item_type", (char*)"channel", (char*)"01/02/2000", (char*)"01/02/2000", (char*)"123456", 500, 345.2, 234.3, 1231.2, 1345.3, 324235.3);
+    extensibleHash.insert(record1, record1.country);
+    extensibleHash.insert(record2, record2.country);
+    extensibleHash.insert(record3, record3.country);
+    extensibleHash.insert(record4, record4.country);
+    SaleRecord record5 = extensibleHash.search(record3.country);
+    cout << record5.country << endl;
+    extensibleHash.remove(record3.country);
+    extensibleHash.remove(record4.country);
+    extensibleHash.insert(record7, record7.country);
+    SaleRecord record6 = extensibleHash.search(record7.country);
+    cout << record6.country << endl;
     return 0;
 }
